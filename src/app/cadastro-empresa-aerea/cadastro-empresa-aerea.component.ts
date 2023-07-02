8c6a7e9d8354bb2f7734ecccfeb7f5896e7578b6
Huidemar Costa <huidemar@gmail.com>
2023-07-02 16:30:47 -0300

    atividade-09
    Realizado a validação nos fomulários de cadastro de empresa área e parceira, colocado uma validação de expressão regular no cadastro de empresa aerea.
    Realizado a validação do botão salvar para habilitar apenas se todos os campos estiverem corretos na tela de cadastro de empresa aerea.
    Realizado o salvamento das informações no LocalStrorage

--- src/app/cadastro-empresa-aerea/cadastro-empresa-aerea.component.ts
+++ src/app/cadastro-empresa-aerea/cadastro-empresa-aerea.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro-empresa-aerea',
@@ -6,5 +7,85 @@ import { Component } from '@angular/core';
   styleUrls: ['./cadastro-empresa-aerea.component.css']
 })
 export class CadastroEmpresaAereaComponent {
-  valorPagoMilhasMask = '9*.99';
+  
+  empresaForm!: FormGroup;
+  listEmpresas: any[] = []; 
+
+  constructor(private formBuilder: FormBuilder) { 
+
+    for (let i = 0; i < 5; i++) {
+      this.listEmpresas.push({
+        id: i,
+        nomeEmpresa: i+' - '+i,
+        dataSaldo: '2023-06-01',
+        saldoMilhas: 100,
+        valorPagoMilhas: 50
+      });
+    }
+
+  }
+
+  ngOnInit() {
+    this.empresaForm = this.formBuilder.group({
+      id: new FormControl(''),
+      nomeEmpresa: ['', [Validators.required, Validators.pattern(/^[^-\#!@%*()+_<>,ºª§]+$/)], this.validaNomeEmpresa],
+      saldoMilhas: new FormControl('', [Validators.required]),
+      valorPagoMilhas: new FormControl('', [Validators.required])
+    });
+  }
+
+  get id(){
+    return this.empresaForm.get('id')!;
+  }
+  get nomeEmpresa(){
+    return this.empresaForm.get('nomeEmpresa')!;
+  }
+  get saldoMilhas(){
+    return this.empresaForm.get('saldoMilhas')!;
+  }
+  get valorPagoMilhas(){
+    return this.empresaForm.get('valorPagoMilhas')!;
+  }      
+
+  submit() {
+    if (this.empresaForm.invalid){
+      return;
+    }
+    console.log('Enviou formulário');
+    const empresa = this.empresaForm.value;
+    // Lógica para salvar a empresa
+    this.listEmpresas.push(empresa);
+    this.resetForm();    
+  }
+
+  editarEmpresa(id: number) {
+    const empresa = this.listEmpresas.find(e => e.id === id);
+    if (empresa) {
+      this.empresaForm.patchValue(empresa);
+    }
+  }
+
+  resetForm() {
+    this.empresaForm.reset();
+    const idControl = this.empresaForm.get('id');
+    if (idControl) {
+      idControl.setValue('');
+    }
+  }
+  
+  validaNomeEmpresa(control: AbstractControl) {
+    const invalidoNomeRegex = /[-\#!@%*()+_<>,ºª§]/;
+    if (invalidoNomeRegex.test(control.value)) {
+      return true 
+    }
+    return null;
+  }
+  hasInvalidPattern() {
+    return this.nomeEmpresa.hasError('pattern');
+  }  
+
+  isFormInvalid(): boolean {
+    const { nomeEmpresa, saldoMilhas, valorPagoMilhas } = this.empresaForm.controls;
+    return nomeEmpresa.invalid || saldoMilhas.invalid || valorPagoMilhas.invalid || this.empresaForm.pristine;
+  }
 }
